a383e9a77225db3703a01c4160a74ab6d5a5ed0f
atinachang <atinachang@gmail.com>
2021-02-12 12:09:35 -0500

    do i like vue or static html better?

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,37 @@
 <template>
 <First />
 <Second />
+<Third />
 </template>
 
 <script>
 import First from './components/First.vue'
 import Second from './components/Second.vue'
+import Third from './components/Third.vue'
 export default {
   name: 'App',
   components: {
     First,
-    Second
+    Second,
+    Third
+  },
+  data() {
+    return {}
+  },
+  mounted() {
+    $(document).ready(function () {
+	$('#nav').localScroll(800);
+	
+	//.parallax(xPosition, speedFactor, outerHeight) options:
+	//xPosition - Horizontal position of the element
+	//inertia - speed to move relative to vertical scroll. Example: 0.1 is one tenth the speed of scrolling, 2 is twice the speed of scrolling
+	//outerHeight (true/false) - Whether or not jQuery should use it's outerHeight option to determine when a section is in the viewport
+	$('#intro').parallax("50%", 0.1);
+	$('#second').parallax("50%", 0.1);
+	$('.bg').parallax("50%", 0.4);
+	$('#third').parallax("50%", 0.3);
+
+})
   }
 }
 </script>
---
+++ src/components/Third.vue
@@ -0,0 +1,34 @@
+<template>
+	<div id="third">
+			<div class="story">
+				<div class="float-left">
+					<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus quam, facilis odit ab sequi, earum,
+						commodi temporibus voluptates itaque cupiditate recusandae. Neque voluptatibus iste quisquam odit maxime
+						blanditiis dicta iure eaque velit soluta similique natus, nemo accusantium accusamus voluptas ratione
+						voluptatum? Dolorum ipsa id totam reiciendis molestiae nisi sunt harum laudantium eveniet recusandae!
+						Excepturi saepe explicabo quasi, illo, amet iusto officiis animi corrupti aspernatur dignissimos ea
+						corporis, praesentium consequatur natus. Harum culpa omnis nemo esse, veritatis porro aspernatur architecto
+						suscipit necessitatibus, sit consequatur provident! Ratione eveniet praesentium qui minima nulla, enim ex
+						molestias, temporibus odio asperiores quia, ab nihil quae?</p>
+				</div>
+			</div>
+			<!--.story-->
+		</div>
+		<!--#third-->
+</template>
+
+<script>
+export default {
+name: 'Third'
+}
+</script>
+
+<style scoped>
+#third {
+  background: url(../assets/121060008_732191107398266_275819840152547857_n.jpg)
+    50% 0 no-repeat fixed;
+  color: white;
+  height: 650px;
+  padding: 100px 0 0 0;
+}
+</style>
